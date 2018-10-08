84163ed8bff2b0bebe220e506a1ecd07f9d8b6c1
csoto31 <43452597+csoto31@users.noreply.github.com>
2018-10-07 18:06:03 -0600

    Template proyecto

---
+++ InventariosCruzRoja/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using InventariosCruzRoja.Models;
+
+namespace InventariosCruzRoja.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "TI-ICR: Inventariado Cruz Roja";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Cruz Roja Costarricence.";
+
+            return View();
+        }
+
+        public IActionResult Territorios()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
