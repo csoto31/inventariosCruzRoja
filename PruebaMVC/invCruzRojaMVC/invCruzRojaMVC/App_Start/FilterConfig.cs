65326689b40f29132bfe6ce4b7ad00efeaef4ffd
Charlie Guzmán <cxalberto@outlook.com>
2018-10-10 17:48:13 -0600

    Proyecto con la estructura de MVC

---
+++ PruebaMVC/invCruzRojaMVC/invCruzRojaMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace invCruzRojaMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
