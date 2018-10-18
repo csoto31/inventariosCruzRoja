ad61c61b55e79b7ae6251e1670a2f8cb5b19d38c
Christian-2018 <43626096+Christian-2018@users.noreply.github.com>
2018-10-17 19:57:27 -0600

    update

--- invCruzRoja/invCruzRoja/Models/Modelo.Designer.cs
+++ invCruzRoja/invCruzRoja/Models/Modelo.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Charlie Guzmán\Desktop\Nueva carpeta\invCruzRoja\invCruzRoja\Models\Model1.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Carmona\Documents\GitHub\inventariosCruzRoja\invCruzRoja\invCruzRoja\Models\Modelo.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
